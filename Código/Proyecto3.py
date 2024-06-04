62d19f54257fcb19ab9bfacf432dbbed845e704c
Dcalvog25 <davidcg2508@gmail.com>
2024-06-03 21:34:02 -0600

    Correcionn Interfaz Grafica
    Se realizo el cambio hecho de la interfaz ya que no se hizo con clases. Ya todos los codigos hasta el momento se basan en clases

--- Código/Proyecto3.py
+++ Código/Proyecto3.py
@@ -2,78 +2,75 @@ import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
 
-# Menu Principal
-def menuInicio():
-    inicio = Tk()
-    inicio.geometry("1000x600")
-    inicio.title("UNO")
-    inicio.resizable(0,0)
+class MenuInicio:
+    def __init__(self):
+        self.inicio = Tk()
+        self.inicio.geometry("1000x600")
+        self.inicio.title("UNO")
+        self.inicio.resizable(0, 0)
 
-    imagen = Image.open("./Fondos/Fondo2.png")
-    imagen = imagen.resize((1000,600))
-    imagen = ImageTk.PhotoImage(imagen)
-    label_imagen = tk.Label(inicio, image=imagen)
-    label_imagen.place(x=0, y=0, relheight=1,relwidth=1)
+        self.imagen = Image.open("./Fondos/Fondo2.png")
+        self.imagen = self.imagen.resize((1000, 600))
+        self.imagen = ImageTk.PhotoImage(self.imagen)
+        self.label_imagen = tk.Label(self.inicio, image=self.imagen)
+        self.label_imagen.place(x=0, y=0, relheight=1, relwidth=1)
 
-    def Salir():
-        inicio.destroy()
+        self.salir = tk.Button(self.inicio, text="Salir", bg="black", font=("Courier New", 15), fg="white", width=10, command=self.salir)
+        self.salir.place(x=5, y=555)
+        self.iniciar = tk.Button(self.inicio, text="Iniciar", bg="black", font=("Courier New", 15), fg="white", width=10, command=self.salirConfig)
+        self.iniciar.place(x=865, y=555)
 
-    def salirConfig():
-        inicio.destroy()
-        config()
+        self.inicio.mainloop()
 
-    salir = tk.Button(text = "Salir", bg = "black", font = ("Courier New", 15), fg = "white", width = 10, command=lambda:Salir())
-    salir.place(x=5, y=555)
-    iniciar = tk.Button(text = "Iniciar", bg = "black", font = ("Courier New", 15), fg = "white", width = 10, command=lambda:salirConfig())
-    iniciar.place(x=865, y=555)
+    def salir(self):
+        self.inicio.destroy()
 
-    inicio.mainloop()
+    def salirConfig(self):
+        self.inicio.destroy()
+        Config()
 
-def config():
-    menu_config = Tk()
-    menu_config.geometry("1000x600")
-    menu_config.title("UNO")
-    menu_config.resizable(0,0)
+class Config:
+    def __init__(self):
+        self.menu_config = Tk()
+        self.menu_config.geometry("1000x600")
+        self.menu_config.title("UNO")
+        self.menu_config.resizable(0, 0)
 
-    imagen = Image.open("./Fondos/FondoConfig.png")
-    imagen = imagen.resize((1000,600))
-    imagen = ImageTk.PhotoImage(imagen)
-    label_imagen = tk.Label(menu_config, image=imagen)
-    label_imagen.place(x=0, y=0, relheight=1,relwidth=1)
+        self.imagen = Image.open("./Fondos/FondoConfig.png")
+        self.imagen = self.imagen.resize((1000, 600))
+        self.imagen = ImageTk.PhotoImage(self.imagen)
+        self.label_imagen = tk.Label(self.menu_config, image=self.imagen)
+        self.label_imagen.place(x=0, y=0, relheight=1, relwidth=1)
 
-    modo = tk.Label(text="MODO DE JUEGO:", bg = "red", font = ("Impact", 22), fg = "black")
-    modo.place(x=135,y=50)
+        self.modo = tk.Label(self.menu_config, text="MODO DE JUEGO:", bg="red", font=("Impact", 22), fg="black")
+        self.modo.place(x=135, y=50)
 
-    imagenC1 = Image.open("./Fondos/Carta1.png")
-    imagenC1 = imagenC1.resize((120,200))
-    imagenC1 = ImageTk.PhotoImage(imagenC1)
-    Solitario = tk.Button(width=120, height=200, image=imagenC1)
-    Solitario.place(x=80, y=120)
+        self.imagenC1 = Image.open("./Fondos/Carta1.png")
+        self.imagenC1 = self.imagenC1.resize((120, 200))
+        self.imagenC1 = ImageTk.PhotoImage(self.imagenC1)
+        self.solitario = tk.Button(self.menu_config, width=120, height=200, image=self.imagenC1)
+        self.solitario.place(x=80, y=120)
 
-    imagenC2 = Image.open("./Fondos/Carta2.png")
-    imagenC2 = imagenC2.resize((120,200))
-    imagenC2 = ImageTk.PhotoImage(imagenC2)
-    jug_2 = tk.Button(width=120, height=200, image=imagenC2)
-    jug_2.place(x=250, y=120)
+        self.imagenC2 = Image.open("./Fondos/Carta2.png")
+        self.imagenC2  = self.imagenC2.resize((120, 200))
+        self.imagenC2 = ImageTk.PhotoImage(self.imagenC2)
+        self.jug_2 = tk.Button(self.menu_config, width=120, height=200, image=self.imagenC2)
+        self.jug_2.place(x=250, y=120)
 
-    imagenC3 = Image.open("./Fondos/Carta3.png")
-    imagenC3 = imagenC3.resize((120,200))
-    imagenC3 = ImageTk.PhotoImage(imagenC3)
-    jug_3 = tk.Button(width=120, height=200, image=imagenC3)
-    jug_3.place(x=80, y=350)
+        self.imagenC3 = Image.open("./Fondos/Carta3.png")
+        self.imagenC3  = self.imagenC3.resize((120, 200))
+        self.imagenC3 = ImageTk.PhotoImage(self.imagenC3)
+        self.jug_3 = tk.Button(self.menu_config, width=120, height=200, image=self.imagenC3)
+        self.jug_3.place(x=80, y=350)
 
-    imagenC4 = Image.open("./Fondos/Carta4.png")
-    imagenC4 = imagenC4.resize((120,200))
-    imagenC4 = ImageTk.PhotoImage(imagenC4)
-    jug_4 = tk.Button(width=120, height=200, image=imagenC4)
-    jug_4.place(x=250, y=350)
+        self.imagenC4 = Image.open("./Fondos/Carta4.png")
+        self.imagenC4  = self.imagenC4.resize((120, 200))
+        self.imagenC4 = ImageTk.PhotoImage(self.imagenC4)
+        self.jug_4 = tk.Button(self.menu_config, width=120, height=200, image=self.imagenC4)
+        self.jug_4.place(x=250, y=350)
 
-    menu_config.mainloop()
+        self.menu_config.mainloop()
 
 
-def pepe():
 
-    print("123344532")
-
-menuInicio()
-
+MenuInicio()
