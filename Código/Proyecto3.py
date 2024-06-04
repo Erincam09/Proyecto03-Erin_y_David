2ca28efaf845d6f130c806f68b8ebc5b9e36b3da
Dcalvog25 <davidcg2508@gmail.com>
2024-06-04 14:01:00 -0600

    Creacion Mazo
    Se realizo la creacion del objeto carta y sus respectivas clases. Clase cartas y clase mazo.

--- Código/Proyecto3.py
+++ Código/Proyecto3.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
+import random
 
 class MenuInicio:
     def __init__(self):
@@ -48,7 +49,7 @@ class Config:
         self.imagenC1 = Image.open("./Fondos/Carta1.png")
         self.imagenC1 = self.imagenC1.resize((120, 200))
         self.imagenC1 = ImageTk.PhotoImage(self.imagenC1)
-        self.solitario = tk.Button(self.menu_config, width=120, height=200, image=self.imagenC1)
+        self.solitario = tk.Button(self.menu_config, width=120, height=200, image=self.imagenC1, command = self.iramodo1)
         self.solitario.place(x=80, y=120)
 
         self.imagenC2 = Image.open("./Fondos/Carta2.png")
@@ -70,6 +71,59 @@ class Config:
         self.jug_4.place(x=250, y=350)
 
         self.menu_config.mainloop()
+    
+    def iramodo1(self):
+        self.menu_config.destroy()
+        OnevsOne()
+
+class Cartas:
+    def __init__(self,color,numero):
+
+        self.color = color
+        self.numero = numero
+        self.ruta = f"./Cartas/{self.color}_{self.numero}.png"
+        print(self.ruta)
+        self.imagen = None
+
+    def construirimagen(self):
+        self.imagen = Image.open(self.ruta)
+        self.imagen = ImageTk.PhotoImage(self.imagen)
+
+class Mazo:
+    def __init__(self):
+        self.mazo = []
+        self.listacolores = ["Rojo","Azul","Verde","Amarilla"]
+        self.listanumeros = [0,1,2,3,4,5,6,7,8,9]
+        self.listacomodines = ["Toma2","Reversa","Bloqueo"]
+
+        self.crearobjetosmazo()
+        
+    def crearobjetosmazo (self):
+        
+        for color in self.listacolores:
+            for numero in self.listanumeros:
+                carta = Cartas(color,numero)
+                self.mazo+=[carta]
+            for comodin in self.listacomodines:
+                carta = Cartas(color,comodin)
+                self.mazo+=[carta]
+
+    def mezclar(self):
+        random.shuffle(self.mazo)
+
+      
+class OnevsOne:
+    def __init__(self):
+        self.juego1 = Tk()
+        self.juego1.attributes("-fullscreen",True)
+        self.juego1.title("Juego 1v1")
+        self.baraja = Mazo()
+        self.baraja.mezclar()
+        
+        
+        
+
+        
 
 
 
